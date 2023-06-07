6ff4cdfe5fa701f0fcb922c6efc43c566b183aa3
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-06-07 10:55:27 +0530

    style and configure expense component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,23 @@
-function ExpenseItem() {
-  return <h2>This is expense</h2>;
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const year = props.date.getFullYear();
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
