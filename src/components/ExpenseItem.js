8842aac2abff459a20dc7775caeebb1fff4b8383
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-06-07 13:46:45 +0530

    splitting components and creating wrapper(containers) components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,16 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "./Card";
 
 function ExpenseItem(props) {
-  const year = props.date.getFullYear();
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-
   return (
-    <div className="expense-item">
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
