43145c4a5d66d57cb75a318ab1a6199afb63729d
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-06-08 15:53:43 +0530

    add filtering and list rendering methods

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -11,14 +11,16 @@ function ExpenseItem(props) {
   };
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}></button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+        <button onClick={clickHandler}></button>
+      </Card>
+    </li>
   );
 }
 
